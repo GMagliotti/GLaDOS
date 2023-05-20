4870af92c8df351cf84d3f9dc74e876bac8034be
GMagliotti <gmagliotti@itba.edu.ar>
2023-05-20 03:22:54 -0300

    Fast compilation script in bash

--- maker.sh
+++ maker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -w /root/Toolchain -d GLaDOS make clean all
-docker exec -w /root -d GLaDOS make clean all
+docker exec -w /root/Toolchain -ti GLaDOS make clean all
+docker exec -w /root -ti GLaDOS make clean all
 
