6b4e0703a4cf887027884766348c6f8cdf79c77a
GMagliotti <gmagliotti@itba.edu.ar>
2023-05-22 20:55:40 -0300

    Enabled GDB debugging (start w/ ./run.sh gdb)

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-qemu-system-x86_64 -audiodev pa,id=foo -hda Image/x64BareBonesImage.qcow2 -m 512
+
+if [[ "$1" = "gdb" ]]; then
+
+ qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+
+else
+
+ qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+
+fi
